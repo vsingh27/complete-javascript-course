54a8115a10b5f8eb3f053857499e68dd5ff4bf51
Vishav Brar <vsingh44@my.bcit.ca>
2018-06-20 19:53:11 -0700

    Display the Dice Result

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,29 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+var diceDom = document.querySelector('.dice');
+var btnRollDom = document.querySelector('.btn-roll');
+diceDom.style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+
+btnRollDom.addEventListener('click', function() {
+
+    //1. Random Number
+    var dice = Math.floor(Math.random() * 6) +1;
+    //2. Display the result
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+    //3. Update Round Score IF the rolled Number was NOT 1
+});
